d6b3fd7ac1f941351e873bc7fbc57f291580b73c
Jonas Grumby <plavita@hotmail.com>
2018-08-27 13:43:03 -0400

    First Push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
