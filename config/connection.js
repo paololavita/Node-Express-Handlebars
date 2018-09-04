157fa6921cc74b6738b576bf72af6ba5c8d5a947
Jonas Grumby <plavita@hotmail.com>
2018-09-04 12:35:36 -0400

    Troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   var connection = mysql.createConnection(process.env.JAWSDB_URL)
+  res.send(JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
       port: 3306,
